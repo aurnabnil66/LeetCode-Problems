32e741e7448c780e64fe3ebaf58e63e56e5a7d12
Aurnab Paul <105810364+aurnabnil66@users.noreply.github.com>
2025-04-06 10:54:36 +0600

    258 best approach added

---
+++ 258. Add_Digits(Best Approach).js
@@ -0,0 +1,15 @@
+let addDigits = function (num) {
+  if (num === 0 || num < 0) {
+    return 0;
+  }
+
+  let result = num % 9 === 0 ? 9 : num % 9;
+
+  return result;
+};
+
+let num = 38;
+
+let result = addDigits(num);
+
+console.log(result);

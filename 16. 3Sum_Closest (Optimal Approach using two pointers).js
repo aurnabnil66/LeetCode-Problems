7264884ae4ef5846bfe0741addf6ac0f1f7ec338
Aurnab Paul <105810364+aurnabnil66@users.noreply.github.com>
2025-09-02 18:23:05 +0600

    3sum closest started

---
+++ 16. 3Sum_Closest (Optimal Approach using two pointers).js
@@ -0,0 +1,11 @@
+let threeSumClosest = function (nums, target) {
+  let sortedNums = nums.sort((a, b) => a - b);
+};
+
+let nums = [-1, 2, 1, -4];
+
+let target = 1;
+
+let result = threeSumClosest(nums, target);
+
+console.log(result);
